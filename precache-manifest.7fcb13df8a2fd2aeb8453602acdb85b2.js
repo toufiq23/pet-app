605acc9117c4ca61794188fe922e6bd15eb25051
toufiq23 <drrameraaron023@gmail.com>
2020-06-01 20:11:52 +0200

    Updates

---
+++ precache-manifest.7fcb13df8a2fd2aeb8453602acdb85b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc3db739cf78f8f59d5460c75c3f556",
+    "url": "/t-petapp/index.html"
+  },
+  {
+    "revision": "c782678a5ef4382dc414",
+    "url": "/t-petapp/static/js/2.4825f39a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t-petapp/static/js/2.4825f39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739d7de09be9cb57d864",
+    "url": "/t-petapp/static/js/main.94a2477d.chunk.js"
+  },
+  {
+    "revision": "56cbf4b137d7a3869564",
+    "url": "/t-petapp/static/js/runtime-main.d4a2e31a.js"
+  }
+]);
